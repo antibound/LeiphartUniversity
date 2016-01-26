b76ff5c7864553b4683675b3206ef2aaf27481cb
Ken Leiphart <antibound@gmail.com>
2016-01-26 14:22:27 -0500

    rgr Generate_Random_Number_Test

---
+++ LeiphartUniversity.Tests/Helpers/UtilitiesTests.cs
@@ -0,0 +1,26 @@
+﻿using LeiphartUniversity.Helpers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeiphartUniversity.Tests
+{
+    [TestFixture]
+    class UtilitiesTests
+    {
+     [TestCase]
+     public void Generate_Random_Number_Test()
+        {
+            Utilities utility = new Utilities();
+            Mock<Random> random = new Mock<Random>();
+
+            random.SetupSequence(m => m.Next(0, 9)).Returns(1).Returns(2).Returns(3).Returns(4).Returns(5).Returns(6).Returns(7).Returns(8).Returns(9).Returns(4);
+
+            Assert.AreEqual(1234567894, utility.GenerateUniversityId(random.Object));
+        }   
+    }
+}
---
+++ LeiphartUniversity/Helpers/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LeiphartUniversity.Helpers
+{
+    public class Utilities
+    {
+        public long GenerateUniversityId(Random random)
+        {
+            //Random random = new Random();
+            StringBuilder newId = new StringBuilder();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                newId.Append(random.Next(0, 9));
+            }
+
+            return long.Parse(newId.ToString());
+        }
+    }
+}
